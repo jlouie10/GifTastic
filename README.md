3c0908110860601539a73a0465ac21eb98cea3dc
justin <jlouie10@users.noreply.github.com>
2019-04-16 15:51:56 -0500

    Added functionality to search button

--- README.md
+++ README.md
@@ -6,6 +6,13 @@ A dynamic web page that populates with gifs of my choice.
 # Screenshots
 
 # What's New
+* Integrated Giphy API
+* Response is parsed and populates images and ratings
+* Introduced array of topics that populates buttons when app is loaded
+* Populates 10 gifs when a topic button is clicked
+* Gifs load as still images until clicked
+* Clicking a gif will toggle its still/animated versions
+* New button created when a topic is searched
 
 # Instructions
 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,132 @@
 $(document).ready(function () {
 
+    /**
+     * Declare Variables
+     */
+
+    var app = {
+        name: "GifTastic",
+        theme: "cartoons",
+        topics: ["Simpsons", "Thundercats", "Tom and Jerry", "South Park", "Spongebob"]
+    }
+
+
+    /**
+     * Run Application
+     */
+
+    getButtons();
+
+
+    /**
+     * Define Functions
+     */
+
+    // Populates topics array buttons
+    function getButtons() {
+        var i;
+
+        for (i = 0; i < app.topics.length; i++) {
+            createButtons(app.topics[i]);
+        }
+    }
+
+    // Creates buttons
+    function createButtons(buttonText) {
+        var button = $("<button>");
+
+        button.text(buttonText)
+            .addClass("get-giphy")
+            .attr("data-value", buttonText);
+
+        $("#buttons").append(button);
+    }
+
+    // Makes request to Giphy API
+    function getGiphy() {
+        var searchString = $(this).attr("data-value");
+        var queryUrl = getQueryUrl(searchString);
+
+        // GET request
+        $.get(queryUrl)
+            .then(function (response) {
+                var results = response.data;
+                var i;
+
+                for (i = 0; i < results.length; i++) {
+                    parseGiphy(results[i], searchString);
+                }
+            });
+    }
+
+    // Concatenantes URL to pass additional parameters
+    function getQueryUrl(search) {
+        var apiKey = "nww7lWBSlBLoaPULkE2UKk8RQP8xyQgm";
+        var endpoint = "http://api.giphy.com/v1/gifs/search";
+        var limit = 10;
+
+        var url = endpoint +
+            "?api_key=" + apiKey +
+            "&q=" + search +
+            "&limit=" + limit;
+
+        return url;
+    }
+
+    // Parses response and creates HTML elements
+    function parseGiphy(object, search) {
+        var img = $("<img>");
+        var p = $("<p>");
+        var still = object.images.fixed_height_still.url;
+        var animate = object.images.fixed_height.url;
+        var rating = object.rating;
+
+        img.attr("src", still)
+            .attr("alt", search)
+            .attr("data-state", "still")
+            .attr("data-still", still)
+            .attr("data-animate", animate);
+
+        p.text("Rating: " + rating);
+
+        $("#images").prepend(p);
+        $("#images").prepend(img);
+    }
+
+    // Updates the state of the gif from still to animate and back to still
+    function updateGif() {
+        var state = $(this).attr("data-state");
+        var still = $(this).attr("data-still");
+        var animate = $(this).attr("data-animate");
+
+        if (state === "still") {
+            $(this).attr("src", animate);
+            $(this).attr("data-state", "animate");
+        }
+        else if (state === "animate") {
+            $(this).attr("src", still);
+            $(this).attr("data-state", "still");
+        }
+        else {
+            console.log("Error");
+        }
+    }
+
+
+    /**
+     * Events
+     */
+
+    $("#add-topic").on("click", function(event) {
+        event.preventDefault();
+
+        var newTopic = $("#gif-input").val();
+
+        app.topics.push(newTopic); // Not sure if this is necessary, topics array serves no added purpose other than populating initial buttons
+
+        createButtons(newTopic);
+    });
+
+    $(document).on("click", ".get-giphy", getGiphy);
+    $(document).on("click", "img", updateGif);
 });
